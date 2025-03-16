4c2b71e98a1dd12b25b3ea9bb27fdaef8125ea45
Grozav Medeea <medeea790@gmail.com>
2025-03-16 12:19:20 +0200

    headmaster open groups

--- frontend/src/pages/DashboardFaculty.jsx
+++ frontend/src/pages/DashboardFaculty.jsx
@@ -4,9 +4,14 @@ import SideNav from "../components/SideNav";
 import AddStudentCard from "../components/AddStudentCard";
 import HeadmasterCard from "../components/HeadmasterCard";
 import GroupCard from "../components/GroupCard";
+import StudentCard from "../components/StudentCard";
+import AddPostCard from "../components/AddPostCard";
+import Button from "../components/Button";
 
 const DashboardFaculty = () => {
   const [selected, setSelected] = useState("Grupe");
+  const [openedGroup, setOpenedGroup] = useState("");
+
   const groups = ["IR1", "IR2", "IR3", "AI1", "IE2"];
   return (
     <>
@@ -18,11 +23,31 @@ const DashboardFaculty = () => {
           setSelected={setSelected}
         />
         <div id="dashboard" className="posts">
-          {groups.map((name, index) => {
-            return <HeadmasterCard name={name} />;
-          })}
-          <AddStudentCard />
+          {openedGroup && (
+            <div className="right center-h">
+              <Button type="roundedButton" action={() => setOpenedGroup("")}>
+                <i class="fa-solid fa-arrow-left"></i>
+              </Button>
+              <h1 className="bigText">{openedGroup}</h1>
+            </div>
+          )}
+          {openedGroup === ""
+            ? groups.map((name, index) => {
+                return (
+                  <HeadmasterCard
+                    name={name}
+                    key={index}
+                    action={() => setOpenedGroup(name)}
+                  />
+                );
+              })
+            : (
+              <GroupCard number={3} students={["Ana", "Maya", "Daniel"]}/>
+            )}
+          {/* <AddStudentCard />
           <GroupCard number={3} />
+          <StudentCard email="maya.grozav06@e-uvt.ro" />
+          <AddPostCard /> */}
         </div>
       </div>
     </>
