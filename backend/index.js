0dbb8a9db1b45ab58444bc6f95417b30774b0369
Parfene Daniel <parfenedaniel345@gmail.com>
2025-03-16 13:36:40 +0200

    BE cors

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const cookieParser = require("cookie-parser");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(cookieParser());
 app.use(express.json());
 app.use("/", router);
