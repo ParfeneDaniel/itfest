051a7446dc419d8e00485cff76cce05710e3f520
RaulSzoboszlai <raulszoboszlai@gmail.com>
2025-03-16 17:39:23 +0200

    dashboard

--- frontend/src/pages/DashboardStudent.jsx
+++ frontend/src/pages/DashboardStudent.jsx
@@ -8,31 +8,48 @@ import Button from "../components/Button";
 import { Link } from "react-router-dom";
 
 const DashboardStudent = () => {
-  const subjects = ["ASDII", "PII", "CDI", "MPI", "LE"];
+  const subjects = ["ASDII", "PII", "CDI", "MPI", "LFTA", "PP", "LE", "EWB", "PV", "PLA"];
   const [searchBarValue, setSearchBarValue] = useState("");
 
   const [selectedSubject, setSelectedSubject] = useState(subjects[0]);
   // const posts = [];
   const posts = [
     {
-      post: "Mă puteți ajuta vă rog la capitolul 4? Nu reușesc să înțeleg listele simplu înlănțuite",
+      post: "Mă puteți ajuta vă rog la capitolul 4? Nu reușesc să înțeleg listele simplu înlănțuite.",
       autor: "Ana Popescu",
       time: "14:53",
+      helper: "Flavius Radin",
+      comment: "Da sigur! Daca te uiti la fisiere pe classroom este un document cu teoria de la liste cu tot cu exemple.",
     },
     {
-      post: "",
-      autor: "Bogdan",
-      time: "3:40",
+      post: "Are cineva idee cum sa aplic criteriul Clestelui la exercitiul 4 din extrasul 2 de la calcul diferential si integral?",
+      autor: "Bogdan Vasilescu",
+      time: "16:40",
+      helper: "George Putnik",
+      comment: "Trebuie sa incadrezi termenul respectiv intre alti doi termeni dupa care treci la limita, iar limita din mijloc da tinde si ea la celelalte doua.",
     },
     {
-      post: "eu nu",
-      autor: "Constantin",
-      time: "3:10",
+      post: "Cum pot mai exact aplica metoda DP la exericitul 5 pentru tema de la Logica Computationala?",
+      autor: "Constantin Horia",
+      time: "12:10",
+      helper: "Tudor Nagy",
+      comment: "Salut! Trebuie sa ai grija la cele 3 reguli. Prima data vezi daca se poate aplica regula literalului pur sau cea a clauzei cu un sigur literal. Daca nu aplici rezolutia.",
     },
     {
-      post: "De ce exista matematica????",
-      autor: "dodo",
-      time: "1:00",
+      post: "Ce proprietate are lambda la LFTA????",
+      autor: "Aron Mihena",
+      time: "10:30",
+    },
+    {
+      post: "Cum verifica daca exista vocale intr-un sir de caractere? Daca imi poate explica cineva va rog pentru ca nu am inteles.",
+      autor: "Blaj Alexandra",
+      time: "22:45",
+    },
+    {
+      post: "Are cineva codul de la exercitiul 2 de la liste circulare facut la seminarul 7?",
+      autor: "Laslo Kevin",
+      time: "19:23",
+      
     },
   ];
 
